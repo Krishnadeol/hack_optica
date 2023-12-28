18a52f5781fe548929a242fe2e6b527283b96fd8
Krishnadeol <114430936+Krishnadeol@users.noreply.github.com>
2023-12-28 23:55:16 +0530

    loader added

--- jscop-app/src/App.js
+++ jscop-app/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useEffect,useState } from 'react';
+import Spiral from './components/Spiral'
 function App() {
+
+  const[Load,setLod]=useState(false);
+  useEffect(()=>{
+    setLod(true);
+    setTimeout(()=>{
+    setLod(false);
+    },8000)
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+    <>
+  {Load ? <Spiral /> : <h1>santa clause</h1>}
+    </>
   );
 }
-
 export default App;
