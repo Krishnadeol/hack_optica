b9531ff7220d410ef37b2436179f3bd840636924
ganesh-7985 <shankarganesh3005@gmail.com>
2023-12-29 09:44:23 +0530

    Horizontal Scorll component

--- jscop-app/src/App.js
+++ jscop-app/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ParticleRing from './components/ParticleRing'
 import Navbar from './components/Navbar'
+import HorizontalScrollCarousel from './components/HorizontalScrollCarousel'
 
 function App() {
   return (
     <div className='bg-[#10172a]'>
     <Navbar/>
      <ParticleRing/>
+     <HorizontalScrollCarousel/>
+     
     </div>
   )
 }
